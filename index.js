9c10b59cdb6b4868bfaafae4264d8d7e45103b50
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2023-05-16 01:11:15 +0600

    update

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ async function run() {
     // start
 
     // admin
-    // admin here========
+    // admin here=========
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === "admin";
+      const isAdmin = user?.role === "admin";
       res.send({ admin: isAdmin });
     });
 
